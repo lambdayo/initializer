06824ddfa7a5bbf4bde84d64fdf03c2077fc8553
Lambdayo <47547047+lambdayo@users.noreply.github.com>
2020-05-14 00:16:29 +0800

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -fsSL -o /etc/yum.repos.d/docker-ce.repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum install -y docker-ce
+systemctl enable docker --now
