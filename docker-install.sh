f8971dd6df48bfc4900f1a7ed6de84669b8db353
Lambdayo <47547047+lambdayo@users.noreply.github.com>
2020-05-21 16:37:39 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -fsSL -o /etc/yum.repos.d/docker-ce.repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum install -y docker-ce
+curl -fsSL -o /etc/yum.repos.d/docker-ce.repo "https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo"
+yum install -y python-pip docker-ce
+pip install docker-compose -i https://pypi.doubanio.com/simple
 systemctl enable docker --now
